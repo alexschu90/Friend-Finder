ef2aaaddb29aae2c984221974327027f00c80e1d
Alex Chu <chu.alex9102@gmail.com>
2019-07-20 16:08:34 -0400

    added test friend to friends array that holds all friends in an array

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var friends = [
+    {
+        name: "Songebob",
+        photo: "https://dailydolphin.hbcsd.org/wp-content/uploads/2017/04/Spongebob-838x900.jpg",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    }
+]
