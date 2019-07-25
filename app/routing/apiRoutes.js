d4ad5ce50461a5f1af19d717c17c5d609080d9f7
Alex Chu <chu.alex9102@gmail.com>
2019-07-24 21:41:51 -0400

    added logig to compare scores

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,13 +7,38 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function(req, res) {
-        var scoreInteters;
-        var currentFriendScores = [];
-        for (let i = 0; i < req.body.scores.length; i++) {
-             scoreInteters = parseInt(req.body.scores[i]);
-             currentFriendScores.push(scoreInteters);
+        match = {
+        name: "",
+        photo: "",
+        friendDifference: 50
+        };
+
+        newFriendData = req.body; 
+        newFriendScores = newFriendData.scores;
+        totalDifference; 
+
+        for (let i = 0; i < friendsData.length; i++) {
+            currentFriend = friendsData[i];
+            var totalDifference = 0; 
+
+            
+            for (let j = 0; j < currentFriend.scores.length; j++) {
+                var currentFriendScore = currentFriend.scores[j];
+                var currentUserScore = newFriendScores[j];
+                intCurrentUser = parseInt(currentUserScore);
+                intCurrentFriend = parseInt(currentFriendScore);
+                totalDifference = totalDifference + Math.abs(intCurrentUser - intCurrentFriend);
+
+                if (totalDifference <= Match.friendDifference) {
+                    Match.name = currentFriend.name;
+                    Match.photo = currentFriend.photo; 
+                    Match.friendDifference = totalDifference;
+                }
+            }
+
+        friends.push(newFriendData);
         }
-        console.log(currentFriendScores);
     });
 };
 
+ 
